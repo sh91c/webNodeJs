b1136d45133bcc876e0b45e93d7cac45f59040c2
sh91c <52781333+sh91c@users.noreply.github.com>
2020-06-07 21:28:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,73 @@
 var http = require("http"); // http, fs, url등은 모듈이다.
-var fs = require("fs"); // 모듈 : nodejs가 갖고있는 수많은 기능들을 비슷한 것기리 묶어놓은 것.
-var url = require("url");
+var fs   = require("fs"); // 모듈들 : nodejs가 갖고있는 수많은 기능들을 비슷한 것끼리 묶어놓은 것.
+var url  = require("url");
 
 var app = http.createServer(function (request, response) {
-    var _url = request.url;
+    var _url      = request.url;
     var queryData = url.parse(_url, true).query; // url을 parse(분석)
-    var title = queryData.id;
+    var pathname  = url.parse(_url, true).pathname;
+    
     console.log(queryData.id);
-    if (_url == "/") {
-        title = "Welcome";
+    // console.log(url.parse(_url, true));
+    //-> URL 정보가 객체에 담겨져있음을 확인할 수 있음
+    // console.log(url.parse(_url, true).pathname);
+
+    if(pathname === '/'){ // 궁금증 : 존재하지 않는 파일경로(쿼리 스트링으로)를 입력했을때 404가 뜨지않고 템플릿이 출력되는 이유가 뭘까...? 일반 경로는 정상적으로 404가 출력이 된다는 것...
+        if(title === undefined){
+            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                    </body>
+                    </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+                var title     = queryData.id;
+                var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                    </body>
+                    </html>
+            `; // 이 템플릿을 기준으로 HTML 구조가 그려진다..
+            response.writeHead(200); // writeHead: 200 - 웹 서버에서 브라우저에게 파일을 성공적으로 전송했다는 뜻
+            response.end(template); // 응답이 완료되었을 때 쿼리 스트링 값에 따라 정보를 출력(또는 어떤 문자열, 데이터등)
+        });}
+    } else {
+    response.writeHead(404); // 404 : 파일을 찾을 수 없다
+    response.end('Not Found.'); 
     }
-    if (_url == "/favicon.ico") {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-    </head>
-    <body>
-    <h1><a href="/">WEB</a></h1>
-    <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ul>
-    <h2>${title}</h2>
-    <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-    <img src="coding.jpg" width="100%">
-    </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-    </p>
-    </body>
-    </html>
-    `;
-    response.end(template); // 쿼리 스트링에 따라 다른 정보를 출력
 
     // 200605 00:57 bak
     // response.end(fs.readFileSync(__dirname + url));
