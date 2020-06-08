a90f72ad3a5e96eefa6c0033cf3469a8b69fc804
sh91c <52781333+sh91c@users.noreply.github.com>
2020-06-08 09:38:48 +0900

    Update readdir.js

--- nodejs/readdir.js
+++ nodejs/readdir.js
@@ -2,5 +2,7 @@ var testFolder = './data/';
 var fs = require('fs');
 
 fs.readdir(testFolder, function(err, filelist){
+    // filelist은 파일목록을 배열형태로 전달
+    // -> 반복문을 통해 처리!
     console.log(filelist);
 })
