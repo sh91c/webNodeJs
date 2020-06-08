d0f37411d70e9f12d06cdb80249f4d69eb9ee6c9
sh91c <52781333+sh91c@users.noreply.github.com>
2020-06-08 09:38:38 +0900

    Update fs.readdir()을 사용해 파일 목록을 불러 링크로 만들기

--- main.js
+++ main.js
@@ -13,10 +13,20 @@ var app = http.createServer(function (request, response) {
     // console.log(url.parse(_url, true).pathname);
 
     if(pathname === '/'){ // 궁금증 : 존재하지 않는 파일경로(쿼리 스트링으로)를 입력했을때 404가 뜨지않고 템플릿이 출력되는 이유가 뭘까...? 일반 경로는 정상적으로 404가 출력이 된다는 것...
-        if(title === undefined){
-            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-                var title = 'Welcome';
+        if(queryData.id === undefined){
+            fs.readdir('./data/', function(err, fileList){
+                var title       = 'Welcome';
                 var description = 'Hello, Node.js';
+
+                // fs.readdir()을 사용해 파일 목록을 불러 링크로 만들기
+                var i     = 0;
+                var list  = '<ul>';
+                while(i < fileList.length){
+                    list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+                    i++;
+                };
+                list     += '</ul>';
+
                 var template = `
                     <!doctype html>
                     <html>
@@ -26,11 +36,7 @@ var app = http.createServer(function (request, response) {
                     </head>
                     <body>
                     <h1><a href="/">WEB</a></h1>
-                    <ul>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ul>
+                    ${list}
                     <h2>${title}</h2>
                     <p>${description}</p>
                     </body>
@@ -40,30 +46,38 @@ var app = http.createServer(function (request, response) {
             response.end(template);
             });
         } else {
-            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-                var title     = queryData.id;
-                var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ul>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ul>
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>
-            `; // 이 템플릿을 기준으로 HTML 구조가 그려진다..
-            response.writeHead(200); // writeHead: 200 - 웹 서버에서 브라우저에게 파일을 성공적으로 전송했다는 뜻
-            response.end(template); // 응답이 완료되었을 때 쿼리 스트링 값에 따라 정보를 출력(또는 어떤 문자열, 데이터등)
-        });}
+            fs.readdir('./data/', function(err, fileList){
+                // fs.readdir()을 사용해 파일 목록을 불러 링크로 만들기
+                var i     = 0;
+                var list  = '<ul>';
+                while(i < fileList.length){
+                    list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+                    i++;
+                };
+                list     += '</ul>'; // 링크 끝
+
+                fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+                    var title     = queryData.id;
+                    var template = `
+                        <!doctype html>
+                        <html>
+                        <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                        </head>
+                        <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${list}
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                        </body>
+                        </html>
+                `; // 이 템플릿을 기준으로 HTML 구조가 그려진다..
+                response.writeHead(200); // writeHead: 200 - 웹 서버에서 브라우저에게 파일을 성공적으로 전송했다는 뜻
+                response.end(template); // 응답이 완료되었을 때 쿼리 스트링 값에 따라 정보를 출력(또는 어떤 문자열, 데이터등)
+                });
+            });
+        }
     } else {
     response.writeHead(404); // 404 : 파일을 찾을 수 없다
     response.end('Not Found.'); 
