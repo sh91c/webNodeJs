c8a3b68b359227b79884cc2a3bf0721e93c3e1fe
sh91c <52781333+sh91c@users.noreply.github.com>
2020-06-10 20:34:56 +0900

    모듈화한 template.js 불러오기

--- main.js
+++ main.js
@@ -3,36 +3,7 @@ var fs   = require("fs");
 var url  = require("url");
 var qs   = require("querystring");
 
-// refactoring
-var template = {
-    HTML : function (title, list, body, control){
-        return `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    ${control}
-                    ${body}
-                    </body>
-                    </html>
-                `;
-    },
-    list : function (fileList){
-        var i     = 0;
-        var list  = '<ul>';
-        while(i < fileList.length){
-            list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-            i++;
-        };
-        list     += '</ul>';
-        return list;
-    }
-}
+var template = require("./lib/template.js");
 
 var app = http.createServer(function (request, response) {
     var _url      = request.url;
